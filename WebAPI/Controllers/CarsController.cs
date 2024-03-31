bbf8992896a1d857b38cca167699e04d8919bef8
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-03-31 14:17:11 +0300

    CarController Added  project

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,30 @@
+﻿ using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.Entity_Framework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            
+            var result = _carService.GetAll();
+            return result.Message;
+        }
+
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<ICarService, CarManager>();
+builder.Services.AddSingleton<ICarDal, EfCarDal>();
 
 var app = builder.Build();
 
